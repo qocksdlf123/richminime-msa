942739a3656f52c796f8d5bee3a250d78f7eef11
qocksdlf123 <qocksdlf123@naver.com>
2024-02-08 19:39:14 +0900

    Feat) spring cloud gateway, gpt 마이크로 서비스 생성

---
+++ chatgpt/src/main/java/com/example/chatgpt/dto/PromptReqDto.java
@@ -0,0 +1,15 @@
+package com.example.chatgpt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PromptReqDto {
+
+    String request;
+}
